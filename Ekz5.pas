f6459af9e1088302402c3482b090f15edc4f743a
Lingarty <lingarty@gmail.com>
2015-12-07 09:48:29 +0300

    Create Ekz5.pas

---
+++ Ekz5.pas
@@ -0,0 +1,50 @@
+program M2;
+ 
+const 
+  n = 3;
+  
+type 
+  matrix = array [1..n, 1..n] of integer;
+
+var 
+  m: matrix;
+  square: boolean;
+
+procedure reading(var a: matrix);
+var
+  i, j: integer;
+begin
+  writeln ('Введите элементы матрицы');
+    for i := 1 to n  do
+      for j := 1 to n do
+        readln (a[i][j]);
+end;
+
+function isMagic(m: matrix): boolean;
+type 
+  line = array [1..n] of integer; //для подсчёта сумм по строкам
+  column = array [1..n] of integer; //для подсчёта сумм по столбцам
+  
+var
+  i, j: integer;
+  lineSum: line;
+  columnSum: column;
+  
+begin
+  for i := 1 to n do
+    for j := 1 to n do
+      begin
+        lineSum[i] := lineSum[i] + m[i,j];
+        columnSum[j] := columnSum[j] + m[i,j];
+      end;
+  for i := 1 to n do
+    if lineSum[i] <> columnSum[i] then isMagic := false
+      else isMagic := true;
+end;
+
+BEGIN
+  reading(m);
+  square := isMagic(m);
+  if square then writeln ('Введённая матрица является магическим квадратом')
+    else writeln ('Введённая матрица не является магическим квадратом')
+END.
