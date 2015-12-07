f82ac4e2b2faa0bbeb7c39eb8c8ef82769cd1190
Lingarty <lingarty@gmail.com>
2015-12-07 10:44:07 +0300

    Create StringAndNumbs.pas

---
+++ StringAndNumbs.pas
@@ -0,0 +1,27 @@
+program string_and_numbs_1;
+
+var
+  simbol: char;
+  flag: boolean;
+  
+procedure reading (var simbol: char);
+begin
+  writeln ('Введите символ');
+  readln(simbol);
+end;
+
+function isNumber(simbol: char): boolean;
+var 
+  flag: boolean;
+begin
+  if (integer(simbol) < 58) and (integer(simbol) > 47) then flag := true
+    else flag := false;
+  isNumber := flag;
+end;
+
+BEGIN
+  reading(simbol);
+  flag := isNumber(simbol);
+  if flag then writeln (integer(simbol))
+    else writeln (-1);
+END.
