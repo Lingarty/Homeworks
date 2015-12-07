8fb7bef4f8d973a500c90e987d24170d38fe50e1
Lingarty <lingarty@gmail.com>
2015-12-07 10:09:28 +0300

    Create Ekz7.pas

---
+++ Ekz7.pas
@@ -0,0 +1,25 @@
+program file_task_2;
+
+var
+  myFile: text;
+  s: integer;
+
+function sumNumbers(f: text): integer;
+var
+  sum, num: integer;
+begin
+  assign(f, 'C:\Users\Альбина\Documents\GitHub\Homeworks\input.txt');
+  reset(f);
+  while not eof(f) do
+    begin
+      read(f, num);
+      sum := sum + num;
+    end;
+  close(f);
+  sumNumbers := sum;
+end;
+
+BEGIN
+  s := sumNumbers(myFile);
+  writeln('Сумма введённых чисел равна ', s);
+END.
