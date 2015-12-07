7ae47e9ca67bc3d234523f1d97efbd1025d1671e
Lingarty <lingarty@gmail.com>
2015-12-07 10:30:48 +0300

    Create Ekz8.pas

---
+++ Ekz8.pas
@@ -0,0 +1,30 @@
+program file_task_3;
+
+const 
+  nmax = 10;
+  
+type
+  number = 1..1000;
+  mas = array [1..nmax] of number;
+
+var
+  a: mas;
+  myFile: text;
+  
+procedure writingFile(var myFile: text; a: mas);
+var
+  i: integer;
+begin
+  assign(myFile, 'C:\Users\Альбина\Documents\GitHub\Homeworks\Output.txt');
+  rewrite(myFile);
+  for i := 1 to nmax do
+    begin
+      read(a[i]);
+      write(myFile, a[i], ' ');
+    end;
+  close(myFile);
+end;
+
+BEGIN
+  writingFile(myFile, a);
+END.
