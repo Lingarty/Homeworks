d3f41298e63ad8c4ffaf1b3204648a6a67bfced4
Lingarty <lingarty@gmail.com>
2015-12-07 09:26:50 +0300

    Create Ekz4.pas

---
+++ Ekz4.pas
@@ -0,0 +1,70 @@
+program M1;
+
+const 
+  n = 3;
+  
+type 
+  matrix = array [1..n, 1..n] of integer;
+
+var 
+  m: matrix;
+
+procedure reading(var a: matrix);
+var
+  i, j: integer;
+begin
+  writeln ('Введите элементы матрицы');
+    for i := 1 to n  do
+      for j := 1 to n do
+        readln (a[i][j]);
+end;
+
+procedure saddlePoints (a: matrix);
+
+type
+  line = array [1..n] of integer;
+  column = array [1..n] of integer;
+  
+var
+  i, j: integer;
+  maxLine, minline: line; //нужны для минимумов и максимумов строк
+  maxColumn, minColumn: column; //нужны для минимумов и максимумов столбцов
+  
+begin
+  for i := 1 to n do //запоминаем столбцы и строки матрицы для дальнейшего сравнения
+    begin
+      maxLine[i] := m[i, 1];
+      minLine[i] := m[i, 1];
+    end;
+  for j := 1 to n do
+    begin
+      maxColumn[j] := m[1, j];
+      minColumn[j] := m[1, j];
+    end;
+    
+  for i := 1 to n do //проводим сравнение элементов и ищем седловые точки
+    for j := 1 to n do
+    begin
+      if m[i, j] < minLine[i] then
+        minLine[i] := m[i, j]
+      else
+      if m[i, j] > maxLine[i] then
+        maxLine[i] := m[i, j];
+      if m[i, j] < minColumn[j] then
+        minColumn[j] := m[i, j]
+      else
+      if m[i, j] > maxColumn[j] then
+        maxColumn[j] := m[i, j];
+    end;
+    
+    writeln ('Седловые точки:');  //Выводим результат
+    for i := 1 to n do
+      for j := 1 to n do
+        if (minLine[i] = maxColumn[j]) or (maxLine[i] = minColumn[j]) then
+          writeln('Элемент ', m[i ,j], ', расположенный в [', i, ';', j, ']');
+end;
+
+BEGIN
+  reading(m);
+  saddlePoints(m);
+END.
