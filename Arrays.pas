0e987e22b0ecad33c3dbc94de0f9545468237b81
Lingarty <lingarty@gmail.com>
2015-12-16 17:04:21 +0300

    Update Arrays.pas

--- Arrays.pas
+++ Arrays.pas
@@ -8,14 +8,13 @@ type
 
 var
   t, s: str;
-  i, j{индексы}, k, l{кол-во символов}, m {ответ}, r {просто коэффициент}: integer;
+  k, l{кол-ва символов}, m {ответ}: integer;
 
+procedure input(var t: str; var k: integer);
+var
+  i: integer;
 begin
   i := 1;
-  k := 0;
-    
-  writeln('Введите строку t, в конце введите "."');
-  
   read(t[i]);
   while (i <> n) do 
     if (t[i] = '.') then break 
@@ -26,26 +25,15 @@ begin
           read(t[i]);
         end; 
   readln();
-  
-  j := 1;
-  l := 0;
-  
-  writeln('Введите строку s, в конце введите "."');
-  
-  read(s[j]);
-  while (j <> n) do 
-    if (s[j] = '.') then break 
-      else 
-        begin
-          inc(j);
-          inc(l);
-          read(s[j]);
-        end; 
-  
+end;
+
+procedure findEqual(var m: integer);
+var
+  i, j {индексы}, r {просто коэффициент}: integer;
+begin
   m := -1;
-  j := 1;
-  r := 0;
-  
+  j := 1; //индекс подстроки
+    
   for i := 1 to k do
     begin
       while (j <= l)do
@@ -64,8 +52,19 @@ begin
           break;
         end;
     end;
+end;
 
+procedure output(var m: integer);
+begin
   if (m = -1) then writeln('s не входит в t')
-  else writeln('Индекс вхождения: ', m - 1);
-  
+  else writeln('Индекс вхождения: ', m - l + 1);
+end;
+
+begin 
+  writeln('Введите строку t, в конце введите "."');
+  input(t, k);
+  writeln('Введите строку s, в конце введите "."');
+  input(s, l);
+  findEqual(m);  
+  output(m);
 end.
