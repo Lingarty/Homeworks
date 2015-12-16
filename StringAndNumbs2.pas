6450ee8f3e373e72bfc0c6cf9357f1617d587607
Lingarty <lingarty@gmail.com>
2015-12-16 19:05:21 +0300

    Update StringAndNumbs2.pas

--- StringAndNumbs2.pas
+++ StringAndNumbs2.pas
@@ -10,36 +10,61 @@ var
   a: massive;
   i: integer;
   flag: boolean;
-  number, numeral, power: real;
+  number: real;
   
-procedure reading (var simbol: char);
+procedure reading (var a: massive);
+var
+  i: integer;
 begin
-  writeln ('Введите символ');
-  readln(simbol);
+  writeln ('Введите набор символов');
+  for i := 1 to nmax do
+    read(a[i]);
 end;
 
-function isNumber(simbol: char): boolean;
+function isNumber(a: massive): boolean;
 var 
-  flag: boolean;
+  flag: array [1..nmax] of boolean;
+  i, q: integer;
 begin
-  if (integer(simbol) < 58) and (integer(simbol) > 47) then flag := true
-    else flag := false;
-  isNumber := flag;
+  //допустим, все символы числа
+  for i := 1 to nmax do
+    flag[i] := true;
+  //если это не так, для первого символа условие изменится
+  for i := 1 to nmax do
+    if (integer(a[i]) > 58) or (integer(a[i]) < 47) then 
+      begin
+        flag[i] := false;
+      end;
+  //проверяем, что условие осталось true для всех элементов
+  for i := 1 to nmax do 
+    if flag[i] = true then inc(q);
+  //выводим результат
+  if q = nmax then result := true
+    else result := false;
+end;
+
+procedure createNumber(var number: real; a: massive);
+var
+  numeral, power: real;
+  i: integer;
+begin
+  for i := 1 to nmax do
+    begin
+      power := exp(ln(10) * (nmax - i));
+      numeral := integer(a[i]) - 48;
+      number := numeral * power + number;
+    end;
+end;
+
+procedure output(number: real);
+begin
+  if number = 0 then writeln (-1)
+     else writeln (number);
 end;
 
 BEGIN
-  for i := 1 to nmax do
-    begin
-      reading(a[i]);
-      flag := isNumber(a[i]);
-      if flag then 
-        begin
-          power := exp(ln(10) * (nmax - i));
-          numeral := integer(a[i]) - 48;
-          number := numeral * power + number;
-        end
-        else break;
-   end;
-   if number = 0 then writeln (-1)
-     else writeln (number);
+  reading(a);
+  flag := isNumber(a);
+  if flag then createNumber(number, a);
+  output(number);
 END.
