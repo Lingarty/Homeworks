33e9c5407edec190b1d67a7c22d7f187843b478e
Lingarty <lingarty@gmail.com>
2015-12-16 11:52:18 +0300

    Update Semestr1.pas

--- Semestr1.pas
+++ Semestr1.pas
@@ -1,19 +1,30 @@
-program semestr1;
+program task;
 
 const
   n = 10;
-
+  
 type
-  mas = array [1..n] of real;
-  korni = array [1..3, 1..n] of real;
+  massive = array [1..n] of real; 
+  solutions = array [1..3, 1..n] of real;
   series = array [1..3] of real;
 
 var
-  x: mas;
-  y: korni;
+  x: massive;
+  y: solutions;
   a: series;
-  b, h, max, fi, psi: real;
-  i, j: integer;
+  b, h, {коэффициенты} fi, ksi: real;
+  
+procedure input(var b, h: real; var a: series);
+var
+  i: integer;
+begin
+  writeln('Введите коэффициенты b и h:');
+  readln(b, h);
+  writeln('Введите 3 значения a');
+  for i := 1 to 3 do
+    readln(a[i]);
+  writeln('- - - - - - - - - - -');
+end;
 
 function F(a, x: real): real;
 begin
@@ -22,53 +33,59 @@ begin
   else writeln('невозможно найти значение функции');
 end;
 
-function fiFunction (y: korni; i: integer): real;
+function fiFunction (i: integer; y: solutions): real;
+var
+  j: integer;
+  fi: real;
 begin
-  fi := 0;
-  
-  for j := 1 to n do //фи для серии
-    fi := fi + y[i][j];
-    
+  for j := 1 to n do
+    fi := fi + y[i][j] * y[i][j];
   fiFunction := fi;
 end;
 
-function psiFunction (y: korni; i: integer): real; 
+function ksiFunction (i: integer; y: solutions): real; 
+var
+  max: real;
+  j: integer;
 begin
   max := y[1][1];
     
-  for j := 1 to n do //пси для серии
+  for j := 1 to n do
   begin
     y[i][j] := y[i][j] * y[i][j];
     if (y[i][j] > max) then max := y[i][j];
   end;
-  
-  psiFunction := max;
+  result := max;
 end;
 
+
+procedure output(i: integer; a, fi, ksi: real);
 begin
-  writeln('Введите b и h - коэффициенты');
-  readln(b, h);
-  writeln('Введите 3 значения a');
+  writeln(i, '-я серия корней:');
+  writeln('a = ', a);
+  writeln('fi = ', fi:0:3);
+  writeln('ksi = ', ksi:0:3);
+  writeln();
+end;
+
+procedure computingSolution (b, h, fi, ksi: real; a: series; var y: solutions; x: massive);
+var
+  i, j: integer;
+begin
   for i := 1 to 3 do
-    readln(a[i]);
-  writeln('- - - - - - - - - - -');
-  
-  for i := 1 to 3 do 
-  begin
-    for j := 1 to n do
     begin
-      x[j] := b + j * h;
-      y[i][j] := F(a[i], x[j]); // посчитали значения функции для серии корней
+      for j := 1 to n do
+        begin
+          x[j] := b + j * h;
+          y[i][j] := F(a[i], x[j]);
+          fi := fiFunction(i, y);
+          ksi := ksiFunction(i, y);
+        end;
+      output(i, a[i], fi, ksi);
     end;
-    
-    fi := fiFunction (y, i);
-    
-    psi := psiFunction (y, i);
-    
-    writeln(i, '-я серия корней:'); //блок вывода
-    writeln('a = ', a[i]);
-    writeln('fi = ', fi:0:3);
-    writeln('psi = ', psi:0:3);
-    writeln();
-  end;
-end.
+end;
+
+BEGIN
+  input(b, h, a);
+  computingSolution(b, h, fi, ksi, a, y, x);
+END.
