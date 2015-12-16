b38edc5623ffc25e6e0a6ae3d0b003845328b80e
Lingarty <lingarty@gmail.com>
2015-12-16 17:40:55 +0300

    Update ArraysKMP.pas

--- ArraysKMP.pas
+++ ArraysKMP.pas
@@ -1,33 +1,21 @@
 program KnutMorrisPrattSearch;
 
-procedure Prefix;
-  var
-    S: array[1..10000] of char;
-    P: array[1..10000] of word; //массив для значений префикс-функции
-    i, k, m: longint;
+type
+  str = array[1..40000] of char;
+  subString = array[1..10000] of char;
+  arrayOfPrefix = array[1..10000] of word; 
   
-  begin
-    P[1] := 0; 
-    k := 0;
-    for i := 2 to m do 
-      begin
-        while (k > 0) and (S[k + 1] <> S[i]) do
-          k := P[k]; 
-          if S[k + 1] = S[i] then
-            k := k + 1;
-        P[i] := k; 
-      end;
-  end;
-
 var
-  T: array[1..40000] of char;
-  S: array[1..10000] of char;
-  P: array[1..10000] of word; //массив для значений префикс-функции
-  i, k: longint;
-  m, n: longint;
+  T: str;             //строка
+  S: subString;      //подстрока
+  P: arrayOfPrefix; //массив значений префикс-функции
+  k {количество символов, совпавших на данный момент}, m, n {длины строк}: longint;
 
+procedure input(var n, m: integer; var T: str; var S: subString);
+var
+  i, j: longint;
 begin
-  writeln('Введите длину строки'); //Блок ввода данных
+  writeln('Введите длину строки');
   readln(n);
   writeln('Введите строку T');
   for i := 1 to n do
@@ -35,19 +23,45 @@ begin
   writeln('Введите длину подстроки');
   readln(m);
   writeln('Введите подстроку');
-  for k := 1 to m do
-    read(S[k]);
-  Prefix; 
-  k := 0; //количество символов, совпавших на данный момент
+  for j := 1 to m do
+    read(S[j]);
+end;
+
+procedure Prefix(var m, k: integer; var P: arrayOfPrefix; var S: subString);
+var
+ i: longint;
+begin
+  P[1] := 0; 
+  for i := 2 to m do 
+    begin
+      while (k > 0) and (S[k + 1] <> S[i]) do
+        k := P[k]; 
+        if S[k + 1] = S[i] then inc(k);
+        P[i] := k; 
+    end;
+end;
+
+procedure output(var k, i: longint; P: arrayOfPrefix);
+begin
+  writeln('Подстрока S входит в текст T, начиная с ', i - m + 1, '-ого символа');
+  k := P[k];
+end;
+
+procedure searchPosition(var n, k: integer; var T: str; var S: subString);
+var
+  i: longint;
+begin
   for i := 1 to n do
     begin
       while (k > 0) and (S[k + 1] <> T[i]) do
         k := P[k];
       if S[k + 1] = T[i] then inc(k);
-    if (k = m) then 
-      begin
-        writeln('Подстрока S входит в текст T, начиная с ', i - m + 1, '-ого символа');
-        k := P[k];
-      end;
+       if (k = m) then output(k, i, P);
     end;
+end;
+
+begin
+  input(n, m, T, S);
+  Prefix(m, k, P, S); 
+  searchPosition(n, k, T, S);
 end.
