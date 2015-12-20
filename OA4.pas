08603053bec8f5b5c3caf6525e93743ac48bcb87
Lingarty <lingarty@gmail.com>
2015-12-20 19:31:13 +0300

    Update OA4.pas

--- OA4.pas
+++ OA4.pas
@@ -1,10 +1,7 @@
 program OA4;
-
-const
-  nmax = 10;
   
 type 
-  massive = array [1..nmax] of real;
+  massive = array of real;
   
 var
   a1, a2, a3: massive;
@@ -14,7 +11,7 @@ procedure readMassive(var a: massive; var n: integer);
 var
   i: integer;
 begin
-  for i := 1 to n do
+  for i := 0 to n - 1 do
     readln (a[i]);
 end;
 
@@ -22,26 +19,31 @@ procedure unionOfElements(var a1, a2, a3: massive; var n, m, k: integer);
 var
   i, q {индекс для a2}: integer;
 begin
-  k := 0;
+  k := n * m;
   q := 1;
-  for i := 1 to n do
+  SetLength(a3, k);
+  k := 0;
+  for i := 0 to n * m - 1 do
     begin
-      while (a2[q] < a1[i]) and (k <= m) do
+      while (a2[q] < a1[i]) do
+        if k <= m then 
         begin
+          begin
+            inc(k);
+            a3[k] := a2[q];
+            inc(q);
+          end;
+          if a2[q] = a1[i] then 
+            inc(q);
           inc(k);
-          a3[k] := a2[q];
-          inc(q);
-        end;
-      if a2[q] = a1[i] then 
-        inc(q);
-      inc(k);
-      a3[k] := a1[i];
-    end;
-  while (q <= m) do
-    begin
-      inc(k);
-      a3[k] := a2[q];
-      inc(q);
+          a3[k] := a1[i];
+        end 
+          else 
+            begin
+              inc(k);
+              a3[k] := a2[q];
+              inc(q);
+            end;
     end;
 end;
 
@@ -49,7 +51,7 @@ procedure printMassive(m: massive; k: integer);
 var
   i: integer;
 begin
-  for i := 1 to k do
+  for i := 0 to k - 1 do
     write (m[i], ' ');
   writeln();
 end;
@@ -57,10 +59,12 @@ end;
 BEGIN
   writeln ('Введите длину первого массива');
   readln(n);
+  SetLength(a1, n);
   writeln ('Введите элементы первого массива (по возрастанию)');
   readMassive(a1, n);
   writeln ('Введите длину второго массива');
   readln(m);
+  SetLength(a2, m);
   writeln ('Введите элементы второго массива (по возрастанию)');
   readMassive(a2, m);
   write ('Объединение массивов: ');
